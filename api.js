e4ebc2093f4cfb0d9dea6e7da8c00687cfa28c9a
Margo1212 <sirachenko944@gmail.com>
2023-06-29 19:28:37 +0200

    add getProduct function

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-import { ScanCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import {
+  ScanCommand,
+  PutItemCommand,
+  GetItemCommand,
+} from "@aws-sdk/client-dynamodb";
 import { unmarshall, marshall } from "@aws-sdk/util-dynamodb";
 import { dbClient } from "./databaseSetup.js";
 import uuid4 from "uuid4";
@@ -52,4 +56,29 @@ const createProduct = async (event) => {
   return response;
 };
 
-export { getAllProducts, createProduct };
+const getProduct = async (event) => {
+  const response = { statusCode: 200 };
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ id: event.pathParameters.id }),
+    };
+    const { Item } = await dbClient.send(new GetItemCommand(params));
+    console.log({ Item });
+    response.body = JSON.stringify({
+      message: "Success;)",
+      data: Item ? unmarshall(Item) : {},
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed;(",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
+
+export { getAllProducts, createProduct, getProduct };
