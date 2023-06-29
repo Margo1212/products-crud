5a587a614f9023edad722445c4b12b8ecfdd2a5b
Margo1212 <sirachenko944@gmail.com>
2023-06-29 18:41:16 +0200

    add getAllProducts function

---
+++ api.js
@@ -0,0 +1,31 @@
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { dbClient } from "./databaseSetup.js";
+
+const getAllProducts = async () => {
+  const response = { statusCode: 200 };
+
+  try {
+    const { Items } = await dbClient.send(
+      new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
+    );
+
+    response.body = JSON.stringify({
+      message: "Success!!;)",
+      data: Items.map((item) => unmarshall(item)),
+      Items,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed;(",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
+
+export { getAllProducts };
